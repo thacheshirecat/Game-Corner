cc61152c61eb71e06eb0f6f675f7217e9baa5981
Cheshire Cat <thacheshirecat@gmail.com>
2018-06-29 10:40:22 -0700

    Add a second test that shows that the single word getter does not ignor case.

--- WordCounter.Tests/ModelTests/WordCounter.Tests.cs
+++ WordCounter.Tests/ModelTests/WordCounter.Tests.cs
@@ -1,6 +1,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WordCounterModel;
-
+//Assign
+//Act
+//Assert
 namespace WordCounterTests
 {
   [TestClass]
@@ -16,6 +18,17 @@ namespace WordCounterTests
       string result = newCount.GetSingleWord();
       //Assert
       Assert.AreEqual(expected, result);
+    }
+    [TestMethod]
+    public void GetSingleWord_DoesNotIgnoreCase_True()
+    {
+      //Assign
+      RepeatCounter newCount = new RepeatCounter("Hello");
+      string expected = "hello";
+      //Act
+      string result = newCount.GetSingleWord();
+      //Assert
+      Assert.AreNotEqual(expected, result);
     }
   }
 
